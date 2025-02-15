616cdc0e81382242137090e76dd28448d24baa8b
pauljako <paulifey18@proton.me>
2025-02-15 21:46:53 +0100

    feat(omp): nix shell

--- sway/lock.sh
+++ sway/lock.sh
@@ -7,6 +7,6 @@ read -r LS < "$LID_STATE_FILE"
 
 case "$LS" in
 *open)   swaymsg output "$LAPTOP_OUTPUT" enable ;;
-*closed) swaymsg output "$LAPTOP_OUTPUT" disable ;;
+*closed) swaylock -i ~/.wallpaper & swaymsg output "$LAPTOP_OUTPUT" disable ;;
 *)       echo "Could not get lid state" >&2 ; exit 1 ;;
 esac
