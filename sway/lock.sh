43dc37fb7c69fbcd63879a247e9667ac2c8d3130
Paul Fey <paulifey18@proton.me>
2025-09-27 20:57:41 +0200

    feat(sway): got a new laptop; things

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-LAPTOP_OUTPUT="LVDS-1"
+LAPTOP_OUTPUT="eDP-1"
 LID_STATE_FILE="/proc/acpi/button/lid/LID0/state"
 
 read -r LS < "$LID_STATE_FILE"
 
 case "$LS" in
 *open)   swaymsg output "$LAPTOP_OUTPUT" enable ;;
-*closed) swaylock -i ~/.wallpaper & swaymsg output "$LAPTOP_OUTPUT" disable ;;
+*closed) swaymsg output "$LAPTOP_OUTPUT" disable ;;
 *)       echo "Could not get lid state" >&2 ; exit 1 ;;
 esac
